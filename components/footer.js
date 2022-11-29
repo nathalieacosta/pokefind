f2af99101e1821d55ec025ed6c5ab771839a3e8f
Nathalie Acosta <nathalieacosta@college.harvard.edu>
2022-11-28 20:47:00 -0500

    remove github

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,7 @@
 export default function Footer() {
   return (
     <div className="bg-slate-700 p-2 fixed inset-x-0 bottom-0 text-center">
-      <span>made with ♡ by nathalie acosta       <a href="https://github.com/nathalieacosta/pokefind">
-        <img
-          width="20"
-          src="https://cdn-icons-png.flaticon.com/512/733/733609.png"
-        ></img>
-      </a></span>
+      <span>made with ♡ by nathalie acosta</span>
     </div>
   );
 }
