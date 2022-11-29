ce18bbb2c56256a9346aa5c9b7f5ac6a4d600b3b
Nathalie Acosta <nathalieacosta@college.harvard.edu>
2022-11-28 21:04:48 -0500

    kofi

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="bg-slate-700 p-2 fixed inset-x-0 bottom-0 text-center">
-      <span>made with ♡ by nathalie acosta</span>
+      <span>made with ♡ by nathalie acosta. <a className="text-blue-400 hover:text-blue-500" href="https://ko-fi.com/nathalieacosta">support me on kofi</a></span>
     </div>
   );
 }
